956b088fc7b4637b8a45b5ea629ba5e5bde7ba4d
Paweł Chabireski <pawel.chabierski@betsys.com>
2018-12-07 13:57:26 +0100

    attempts

--- src/main/scala/com/chabior/seven/App.scala
+++ src/main/scala/com/chabior/seven/App.scala
@@ -4,6 +4,7 @@ object App {
   def main(args: Array[String]): Unit = {
     val input = InputParser.parse
     println(input)
+    println(NodeAnalyze.getPathMulti(input, List(0, 2, 1, 3, 4)))
     println(NodeAnalyze.getPath(input).mkString)
   }
 }
--- src/main/scala/com/chabior/seven/NodeAnalyze.scala
+++ src/main/scala/com/chabior/seven/NodeAnalyze.scala
@@ -8,10 +8,55 @@ object NodeAnalyze {
           case Some(z) => z._1
           case None => throw  new RuntimeException
         }
-        println(graph.map(x => x._1 -> x._2.filterNot(_ == parent)) - parent)
+
         r(graph.map(x => x._1 -> x._2.filterNot(_ == parent)) - parent, path:+parent)
       }
     }
     r(graph, List.empty[Char])
+  }
+
+  def getPathMulti(graph: Map[Char, List[Char]], workers: List[Int]): Int = {
+    var step = 0
+    def r(graph: Map[Char, List[Char]], path: Map[Char, Int], step: Int): Map[Char, Int] = {
+      if (graph.isEmpty) path else {
+        val parents = graph.filter(x => x._2.isEmpty).toList.sortBy(_._1).take(workers.length)
+        println(parents)
+        var subgraph = graph
+        for (parent <- parents) {
+          subgraph = subgraph - parent._1
+        }
+
+        var subpath = path
+        var d = step
+        for (parent <- parents) {
+          var ok = subpath
+          if (ok.get(parent._1) match {
+            case Some(_) => false
+            case None => true
+          }) {
+            ok = ok + (parent._1 -> d)
+          }
+          d = gefNodeDur(parent._1, d)
+          val s = r(subgraph.map(x => x._1 -> x._2.filterNot(_ == parent._1)), ok, d)
+          for ((k, c) <- s) {
+            subpath = subpath.get(k) match {
+              case Some(_) => subpath
+              case None => subpath + (k -> c)
+            }
+          }
+          for ((k, c) <- s) {
+            subgraph = subgraph.map(x => x._1 -> x._2.filterNot(_ == k))
+          }
+        }
+        subpath
+      }
+    }
+    val data = r(graph, Map.empty[Char, Int], step)
+    data.toList.sortBy(_._2).map(_._2).max - 1
+  }
+
+  def gefNodeDur(node: Char, add: Int): Int = {
+    val a = 'A' to 'Z'
+    60 + a.indexOf(node) + 1 + add
   }
 }
